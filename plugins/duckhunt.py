cbdd50f187bfaf25b5bb3ea483cd9bee5fcb657f
LO Kam Tao Leo <leolo@leolo.org>
2020-08-11 06:38:46 +0800

    Fixing bug

--- plugins/duckhunt.py
+++ plugins/duckhunt.py
@@ -923,7 +923,7 @@ def ducks_user(text, nick, chan, conn, db, message):
             "\x02{}'s\x02 goose stats: \x02{}\x02 killed and \x02{}\x02 befriended in {}. "
             "Across {}: \x02{}\x02 killed and \x02{}\x02 befriended. "
             "Averaging \x02{}\x02 and \x02{}\x02 per channel.".format(
-                name, ducks["chankilled"], ducks["chankilled"],
+                name, ducks["chankilled"], ducks["chanfriends"],
                 chan, pluralize_auto(ducks["chans"], "channel"),
                 ducks["killed"], ducks["friend"],
                 pluralize_auto(kill_average, "kill"), pluralize_auto(friend_average, "friend")
