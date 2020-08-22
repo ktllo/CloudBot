365611b7e3f909fb6d010f69fec7f136ac3bc7ae
LO Kam Tao Leo <leolo@leolo.org>
2020-08-22 17:19:23 +0800

    Trying to fix the bug

--- plugins/misc.py
+++ plugins/misc.py
@@ -26,5 +26,5 @@ def ping(text, reply):
     return "Pong"
 
 @hook.command("feed","feeds")
-def feed(text, nick, chan, db, event, is_nick_valid):
-    return "\x01 actiontakes a 3 pounds steak from "+event.conn.nick+" and eat it\x01";
+def feed(nick, action):
+    action("takes a 3 pounds steak from "+nick+" and eat it");
